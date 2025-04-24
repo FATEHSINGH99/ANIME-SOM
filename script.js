0f9bfbe78ba6c9fe4d71acd35e64028f657a76e1
FATEHSINGH99 <monusr502@gmail.com>
2025-04-24 19:07:38 +0530

    Update script.js
    Signed-off-by: FATEHSINGH99 <monusr502@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// पुराना कोड (हटाएँ)
+// const response = await fetch(`https://api.jikan.moe/v4/anime?q=${query}&sfw`);
 
+// नया कोड (CORS Proxy के साथ)
+const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.jikan.moe/v4/anime?q=${query}&sfw`);
